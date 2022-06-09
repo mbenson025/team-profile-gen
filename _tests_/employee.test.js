d2c5cc609bb070d7c4e1f33371541ec45a5c5257
mbenson025 <mbenson025@gmail.com>
2022-06-08 19:25:25 -0500

    setting up lib files to test

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -1,7 +1,11 @@
 const Employee = require('../lib/employee');
 
 test('generate employee object', () => {
-  const testEmp = new Employee();
+  const newEmp = new Employee('john', '1234', 'john@test.com');
 
-  expect(typeof testEmp).toBe('object');
+  expect(typeof newEmp).toBe('object');
+  expect(newEmp.name).toBe('john');
+  expect(newEmp.id).toBe('1234');
+  expect(newEmp.email).toBe('john@test.com');
+  expect(newEmp.getRole()).toBe('Employee');
 });
