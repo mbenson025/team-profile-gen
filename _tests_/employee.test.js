9f90807b90c5c9e9ceff65008d238ced92df4487
mbenson025 <mbenson025@gmail.com>
2022-06-09 23:44:08 -0500

    all employee tests passed

--- _tests_/employee.test.js
+++ _tests_/employee.test.js
@@ -4,8 +4,32 @@ test('generate employee object', () => {
   const newEmp = new Employee('john', '1234', 'john@test.com');
 
   expect(typeof newEmp).toBe('object');
-  expect(newEmp.name).toBe('john');
-  expect(newEmp.id).toBe('1234');
-  expect(newEmp.email).toBe('john@test.com');
-  expect(newEmp.getRole()).toBe('Employee');
+});
+
+test('generate employee name', () => {
+  const name = 'john';
+  const newEmp = new Employee(name);
+
+  expect(newEmp.name).toBe(name);
+});
+
+test('generate employee id', () => {
+  const id = '25';
+  const newEmp = new Employee('john', '25', 'john@test.com');
+
+  expect(newEmp.id).toBe(id);
+});
+
+test('generate employee email', () => {
+  const email = 'john@test.com';
+  const newEmp = new Employee('john', '25', 'john@test.com');
+
+  expect(newEmp.email).toBe(email);
+});
+
+test('generate role', () => {
+  const role = 'Employee';
+  const newEmp = new Employee('john', '25', 'john@test.com');
+
+  expect(newEmp.getRole()).toBe(role);
 });
--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
+
+//class paths
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
