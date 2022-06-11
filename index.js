ba5a51fe4cbfc53bc5d1e1e86865ef2fab33302c
mbenson025 <mbenson025@gmail.com>
2022-06-10 23:44:57 -0500

    validation added to prompts

--- index.js
+++ index.js
@@ -17,21 +17,49 @@ const questions = () => {
         type: 'input',
         name: 'managerName',
         message: "Enter the name of the team's manager",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Manager name is required';
+          }
+        },
       },
       {
         type: 'input',
         name: 'managerId',
         message: 'What is the ID for the manager?',
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'ID is required';
+          }
+        },
       },
       {
         type: 'input',
         name: 'managerEmail',
         message: "What is the manager's email?",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Please enter a valid email';
+          }
+        },
       },
       {
         type: 'input',
         name: 'officeNumber',
         message: 'Enter the office number for this manager',
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Office number is required to proceed';
+          }
+        },
       },
     ])
     .then((userInput) => {
@@ -52,21 +80,49 @@ const engineerPrompt = () => {
         type: 'input',
         name: 'engineerName',
         message: "Enter the engineer's name",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Engineer name is required';
+          }
+        },
       },
       {
         type: 'input',
         name: 'engineerId',
         message: "Enter the engineer's ID",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'ID is required';
+          }
+        },
       },
       {
         type: 'input',
         name: 'engineerEmail',
         message: 'Enter an email for this engineer',
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Please enter a valid email';
+          }
+        },
       },
       {
         type: 'input',
         name: 'engineerGitHub',
         message: 'What is the github username for this engineer',
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Github account username is required';
+          }
+        },
       },
     ])
     .then((userInput) => {
@@ -87,21 +143,49 @@ const internPrompt = () => {
         type: 'input',
         name: 'internName',
         message: "Enter the intern's name",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Intern name is required';
+          }
+        },
       },
       {
         type: 'input',
         name: 'internId',
         message: "Enter the intern's ID",
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'ID is required';
+          }
+        },
       },
       {
         type: 'input',
         name: 'internEmail',
         message: 'Enter the email for this intern',
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Please enter a valid email';
+          }
+        },
       },
       {
         type: 'input',
         name: 'internSchool',
         message: 'What school did the intern graduate from?',
+        validate: (value) => {
+          if (value) {
+            return true;
+          } else {
+            return false, 'Please enter a valid school';
+          }
+        },
       },
     ])
     .then((userInput) => {
