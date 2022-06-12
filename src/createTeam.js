db7ac83f694197e7d98a123f33488dbe9aca4c0d
mbenson025 <mbenson025@gmail.com>
2022-06-12 00:21:28 -0500

    all 3 team roles generating html

--- src/createTeam.js
+++ src/createTeam.js
@@ -46,6 +46,28 @@ const engineerGen = (engineer) => {
   `;
 };
 
+const internGen = (intern) => {
+  return `
+  <div class="col-lg-3 col-md-4 col-sm-6">
+  <div class="my-4 card">
+    <div class="card-header bg-dark text-white">
+      <h3 class="card-title">${intern.name}</h3>
+      <h4 class="card-subtitle">Intern</h4>
+    </div>
+    <div class="card-body bg-light p-3">
+      <ul class="list-group">
+        <li class="list-group-item">ID: ${intern.id}</li>
+        <li class="list-group-item">
+          Email: <a href="mailto:${intern.email}">${intern.email}</a>
+        </li>
+        <li class="list-group-item">School: ${intern.school}</li>
+      </ul>
+    </div>
+  </div>
+</div>
+  `;
+};
+
 const createTeam = (teamArr) => {
   teamList = [];
   for (var i = 0; i < teamArr.length; i++) {
